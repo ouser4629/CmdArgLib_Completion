b52f93fa330f3da87f2344b960957a680b9e3114
Peter <p.summerland@gmail.com>
2026-02-04 11:09:57 -0500

    update to use cmd-arg-lib 0.4.0

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@
 import PackageDescription
 
 // --- Remoteand local - cmd-arg-lib
-//let cmdArgLib = "cmd-arg-lib"
+let cmdArgLib = "cmd-arg-lib"
 
 /// --- Local - source
-let cmdArgLib = "Cmd_Arg_Lib"
+//let cmdArgLib = "Cmd_Arg_Lib"
 
 let package = Package(
     name: "CompletionExample",
@@ -34,9 +34,9 @@ let package = Package(
     ],
     dependencies: [
         // Remote
-        // .package(url: "https://github.com/ouser4629/cmd-arg-lib.git", from: "0.3.9")
+         .package(url: "https://github.com/ouser4629/cmd-arg-lib.git", from: "0.3.9")
         // Local
-         .package(path: "../../\(cmdArgLib)"),
+        // .package(path: "../../\(cmdArgLib)"),
     ],
     targets: [
         .target(
